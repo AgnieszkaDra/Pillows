4a26f9e7ad357495ab11a7552e075de94b0f2d47
Agnieszka Dragaczyk <Agnieszka.127@interia.pl>
2021-04-27 16:59:28 +0200

    add script to section__good

---
+++ JS/app.js
@@ -0,0 +1,14 @@
+let today = new Date();
+let hourNow = today.getHours();
+let greeting;
+
+if (hourNow > 18){
+    greeting = 'Dobry wieczór!';
+} else if (hourNow > 12){
+    greeting = 'Dzień dobry!';
+} else if (hourNow > 0){
+    greeting = 'Dzień dobry!';
+} else {
+    greeting = 'Witamy!';
+}
+document.write('<h3>' + greeting + '</h3>');
